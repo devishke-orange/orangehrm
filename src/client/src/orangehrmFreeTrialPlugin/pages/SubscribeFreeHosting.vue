c2d34d75f2e56ffdda783f63079ce2f55efa852a
ManulMax <manul@orangehrm.us.com>
2023-06-07 10:28:29 +0530

    OHRM5X-2210: Add subscribe form

---
+++ src/client/src/orangehrmFreeTrialPlugin/pages/SubscribeFreeHosting.vue
@@ -0,0 +1,105 @@
+<!--
+/*
+ * This file is part of OrangeHRM Inc
+ *
+ * Copyright (C) 2020 onwards OrangeHRM Inc
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see  http://www.gnu.org/licenses
+ */
+-->
+
+<template>
+  <div class="orangehrm-background-container">
+    <div class="orangehrm-paper-container">
+      <div class="orangehrm-header-container">
+        <oxd-text tag="h6" class="orangehrm-main-title"
+          >Subscribe For Free Hosting</oxd-text
+        >
+      </div>
+      <div>
+        <oxd-divider />
+        <oxd-form>
+          <oxd-form-row>
+            <oxd-grid :cols="1" class="orangehrm-full-width-grid">
+              <oxd-grid-item>
+                <oxd-input-field label="System Url" disabled />
+              </oxd-grid-item>
+            </oxd-grid>
+          </oxd-form-row>
+          <oxd-form-row>
+            <oxd-grid :cols="1" class="orangehrm-full-width-grid">
+              <oxd-grid-item>
+                <oxd-input-field label="Company Name" />
+              </oxd-grid-item>
+            </oxd-grid>
+          </oxd-form-row>
+          <oxd-form-row>
+            <oxd-grid :cols="1" class="orangehrm-full-width-grid">
+              <oxd-grid-item>
+                <oxd-input-field label="No. of Employees" />
+              </oxd-grid-item>
+            </oxd-grid>
+          </oxd-form-row>
+          <oxd-form-row>
+            <oxd-grid :cols="1" class="orangehrm-full-width-grid">
+              <oxd-grid-item>
+                <oxd-input-field label="Country" />
+              </oxd-grid-item>
+            </oxd-grid>
+          </oxd-form-row>
+          <oxd-form-row>
+            <oxd-grid :cols="1" class="orangehrm-full-width-grid">
+              <oxd-grid-item>
+                <oxd-input-field label="Contact Person Name" />
+              </oxd-grid-item>
+            </oxd-grid>
+          </oxd-form-row>
+          <oxd-form-row>
+            <oxd-grid :cols="1" class="orangehrm-full-width-grid">
+              <oxd-grid-item>
+                <oxd-input-field label="Contact Number" />
+              </oxd-grid-item>
+            </oxd-grid>
+          </oxd-form-row>
+          <oxd-form-row>
+            <oxd-grid :cols="1" class="orangehrm-full-width-grid">
+              <oxd-grid-item>
+                <oxd-input-field label="Email" />
+              </oxd-grid-item>
+            </oxd-grid>
+          </oxd-form-row>
+          <div class="orangehrm-action-button">
+            <oxd-form-actions>
+              <oxd-button
+                type="button"
+                display-type="ghost"
+                label="Cancel"
+                @click="onCancel"
+              />
+              <submit-button label="Submit" />
+            </oxd-form-actions>
+          </div>
+        </oxd-form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SubscribeFreeHosting',
+};
+</script>
+
+<style src="./subscribe-free-hosting.scss" lang="scss" scoped></style>
