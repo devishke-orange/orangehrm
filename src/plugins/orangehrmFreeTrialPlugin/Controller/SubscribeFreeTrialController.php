95a1acf48dd86f9a8c236b1a8162d9d115b3c250
ManulMax <manul@orangehrm.us.com>
2023-06-07 10:28:29 +0530

    OHRM5X-2210: Add subscribe form

--- src/plugins/orangehrmFreeTrialPlugin/Controller/SubscribeFreeTrialController.php
+++ src/plugins/orangehrmFreeTrialPlugin/Controller/SubscribeFreeTrialController.php
@@ -1,3 +1,4 @@
+<?php
 /**
  * OrangeHRM is a comprehensive Human Resource Management (HRM) System that captures
  * all the essential functionalities required for any enterprise.
@@ -13,12 +14,23 @@
  *
  * You should have received a copy of the GNU General Public License along with this program;
  * if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
- * Boston, MA  02110-1301, USA
+ * Boston, MA 02110-1301, USA
  */
-import RemainingDays from '@/orangehrmFreeTrialPlugin/components/RemainingDays.vue';
-import SubscribeFreeHosting from '@/orangehrmFreeTrialPlugin/pages/SubscribeFreeHosting.vue';
 
-export default {
-  'remaining-days': RemainingDays,
-  'subscribe-free-hosting': SubscribeFreeHosting,
-};
+namespace OrangeHRM\FreeTrial\Controller;
+
+use OrangeHRM\Core\Controller\AbstractVueController;
+use OrangeHRM\Core\Vue\Component;
+use OrangeHRM\Framework\Http\Request;
+
+class SubscribeFreeTrialController extends AbstractVueController
+{
+    /**
+     * @inheritDoc
+     */
+    public function preRender(Request $request): void
+    {
+        $component = new Component('subscribe-free-hosting');
+        $this->setComponent($component);
+    }
+}
